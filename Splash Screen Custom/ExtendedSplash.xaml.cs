1355e42e9ab0d97f09dd740867dc37c833fa8435
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2022-05-05 20:34:49 -0500

    Custom Splash (ProgressRing is Collapse)

---
+++ Splash Screen Custom/ExtendedSplash.xaml.cs
@@ -0,0 +1,122 @@
+﻿using Splash_Screen_Custom.Common;
+using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Splash_Screen_Custom
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ExtendedSplash : Page
+    {
+        internal Rect splashImageRect; // Rect to store splash screen image coordinates.
+        private SplashScreen splash; // Variable to hold the splash screen object.
+        internal bool dismissed = false; // Variable to track splash screen dismissal status.
+        internal Frame rootFrame;
+
+        // Define methods and constructor
+        public ExtendedSplash(SplashScreen splashscreen, bool loadState)
+        {
+            this.InitializeComponent();
+            DismissExtendedSplash();
+
+            // Listen for window resize events to reposition the extended splash screen image accordingly.
+            // This ensures that the extended splash screen formats properly in response to window resizing.
+            Window.Current.SizeChanged += new WindowSizeChangedEventHandler(ExtendedSplash_OnResize);
+
+            splash = splashscreen;
+            if (splash != null)
+            {
+                // Register an event handler to be executed when the splash screen has been dismissed.
+                splash.Dismissed += new TypedEventHandler<SplashScreen, Object>(DismissedEventHandler);
+
+                // Retrieve the window coordinates of the splash screen image.
+                splashImageRect = splash.ImageLocation;
+                PositionImage();
+
+                // If applicable, include a method for positioning a progress control.
+                PositionRing();
+            }
+
+            //Restore the saved session state if necessary
+            //RestoreState(loadState);
+
+            // Create a Frame to act as the navigation context
+            rootFrame = new Frame();
+
+        }
+
+        async void RestoreState(bool loadState)
+        {
+            if (loadState)
+            {
+                await SuspensionManager.RestoreAsync();
+                // code to load your app's state here
+            }
+        }
+        void PositionImage()
+        {
+            extendedSplashImage.SetValue(Canvas.LeftProperty, splashImageRect.X);
+            extendedSplashImage.SetValue(Canvas.TopProperty, splashImageRect.Y);
+            extendedSplashImage.Height = splashImageRect.Height;
+            extendedSplashImage.Width = splashImageRect.Width;
+        }
+
+        void PositionRing()
+        {
+            splashProgressRing.SetValue(Canvas.LeftProperty, splashImageRect.X + (splashImageRect.Width * 0.5) - (splashProgressRing.Width * 0.5));
+            splashProgressRing.SetValue(Canvas.TopProperty, (splashImageRect.Y + splashImageRect.Height + splashImageRect.Height * 0.1));
+        }
+
+        void ExtendedSplash_OnResize(object sender, WindowSizeChangedEventArgs e)
+        {
+            // Safely update the extended splash screen image coordinates. This function will be executed when a user resizes the window.
+            if (splash != null)
+            {
+                // Update the coordinates of the splash screen image.
+                splashImageRect = splash.ImageLocation;
+                PositionImage();
+
+                // If applicable, include a method for positioning a progress control.
+                PositionRing();
+            }
+        }
+
+        void DismissedEventHandler(SplashScreen sender, object e)
+        {
+            dismissed = true;
+
+            // Complete app setup operations here...
+        }
+
+        private async void DismissExtendedSplash()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(3));  //Delay for 3 seconds
+
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                rootFrame = new Frame();
+                rootFrame.Content = new MainPage();
+                Window.Current.Content = rootFrame;
+            });
+
+            //rootFrame = new Frame();
+            //MainPage mainPage = new MainPage();
+            //rootFrame.Content = mainPage;
+
+            // Navigate to mainpage
+            //rootFrame.Navigate(typeof(MainPage));
+            // Place the frame in the current Window
+            //Window.Current.Content = rootFrame;
+            //rootFrame.Navigate(typeof(MainPage));
+        }
+
+    }
+}
